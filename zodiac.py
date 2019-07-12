2557522f4464c286b7ec621cc2d514d9f9b34611
com <walls.jyron@gmail.com>
2019-07-12 01:32:45 -0700

    FIxing database errors

--- zodiac.py
+++ zodiac.py
@@ -1,15 +1,15 @@
 import os
 from flask import Flask, render_template, request
-
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_file = "sqlite///{}".format(os.path.join(project_dir, "zodiac.db"))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "zodiac.db"))
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 db = SQLAlchemy(app)
-db.init_app(app)
+migrate = Migrate(app, db)
 
 
 #models
