63fe502b45ca44461b5efe8522e80d41a6c62a9a
com <walls.jyron@gmail.com>
2019-07-11 19:06:38 -0700

    creating database

--- zodiac.py
+++ zodiac.py
@@ -1,6 +1,20 @@
+import os
 from flask import Flask, render_template, request
 
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite///{}".format(os.path.join(project_dir, "zodiac.db"))
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+db = SQLAlchemy(app)
+
+#models
+
+class User(db.Model):
+    email = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+    birthday = db.Column(db.)
 
 #routes 
 
